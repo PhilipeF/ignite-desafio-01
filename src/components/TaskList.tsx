2ca63b6f703f07e9e27311997032069d1213ee25
rick <philipesferreiraa@gmail.com>
2023-01-10 01:31:05 -0300

    Altere entre  ou  o campo  de uma task com dado ID

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -37,11 +37,11 @@ export function TaskList() {
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
 
-    // const checkedTask = tasks.map(task => task.id === id ? {
-    //   ...task,
-    //   isComplete: !task.isComplete
-    // } : task)
-    // setTasks(checkedTask)
+     const checkedTask = tasks.map(task => task.id === id ? {
+       ...task,
+       isComplete: !task.isComplete
+     } : task)
+     setTasks(checkedTask)
   }
 
 
