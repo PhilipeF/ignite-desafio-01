b8ecca67e76cc280c9e204610954c9d64784d6eb
rick <philipesferreiraa@gmail.com>
2023-01-14 14:41:31 -0300

    Adcionando README na aplicação

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,10 +14,8 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask(event: any) {
+  function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-
-    event.preventDefault();
 
     if (!newTaskTitle) {
       return;
@@ -31,21 +29,17 @@ export function TaskList() {
 
     setTasks(prev => [...prev, newTask])
     setNewTaskTitle('')
-
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
 
-     const checkedTask = tasks.map(task => task.id === id ? {
-       ...task,
-       isComplete: !task.isComplete
-     } : task)
-     setTasks(checkedTask)
+    const checkedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+    setTasks(checkedTask)
   }
-
-
-
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
